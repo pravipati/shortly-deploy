4605a789b1ad4f5a94ffe3d62491f164a4328fb8
pravipati <psravipati@gmail.com>
2015-03-04 16:22:25 -0800

    Implemented grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['public/client/*.js','public/lib/*.js' ],
+        dest: 'public/dist/built.js',
+      }
     },
 
     mochaTest: {
@@ -21,6 +28,11 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      my_target: {
+      files: {
+        'public/dist/output.min.js': 'public/dist/built.js'
+      }
+    }
     },
 
     jshint: {
@@ -93,7 +105,7 @@ module.exports = function(grunt) {
     'mochaTest'
   ]);
 
-  grunt.registerTask('build', [
+  grunt.registerTask('build', ['concat', 'uglify'
   ]);
 
   grunt.registerTask('upload', function(n) {
